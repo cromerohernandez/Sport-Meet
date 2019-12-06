ae39627e59addb886c905727c9f43ab5565bb42d
cromerohernandez <c.romero.hsv@gmail.com>
2019-12-06 12:54:35 +0100

    add: Model-Match | dev: ModelRequest | fix: Model-SportField

--- models/request.model.js
+++ models/request.model.js
@@ -1,7 +1,11 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const requestSchema = new Schema({
+  reference: {
+    type: Number,
+    required: [true, 'Reference is required']
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
